2ae26d3fad720262615cfca973d350d6546919d0
Conchylicultor <etiennefg.pot@gmail.com>
2015-10-03 12:41:17 +0200

    "Implement leastSquaresGD"

--- leastSquaresGD.m
+++ leastSquaresGD.m
@@ -2,6 +2,33 @@ function [ beta ] = leastSquaresGD( y,tX,alpha )
 %leastSquaresGD Least squares using gradient descent
 %   Remark: alpha is the step-size
 
+    % TODO: Implement stochastic gradient descent
+    % QUESTION: Is least square the same as mean square error ???
+
+    % Parameters
+    maxIters = 1000;
+
+    % Initialization
+    beta = [0; 0];
+
+    % Main loop
+    for k = 1:maxIters
+        
+        g = computeGradientMSE(y, tX, beta); % Compute gradient
+        beta = beta - alpha .* g; % Update beta
+
+        % For debugging
+        L = costMSE(y, tX, beta);
+        fprintf('%.2f  %.2f %.2f\n', L, beta(1), beta(2));
+    end
 
 end
 
+function [ g ] = computeGradientMSE( y, tX, beta )
+%computeGradientMSE Compute the gradient for mean square error
+
+    N = length(y);
+    e = y - tX*beta;
+    g = -1/N * tX'*e;
+
+end
