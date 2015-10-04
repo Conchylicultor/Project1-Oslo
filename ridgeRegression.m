4cf2248fa3891453d8270a0aa6a38ce129932ca4
Conchylicultor <etiennefg.pot@gmail.com>
2015-10-04 15:59:27 +0200

    ridgeRegression implementation

--- ridgeRegression.m
+++ ridgeRegression.m
@@ -2,6 +2,9 @@ function [ beta ] = ridgeRegression( y,tX, lambda )
 %ridgeRegression Ridge regression using normal equations.
 %   Remark: lambda is the regularization coefficient.
 
-
+    I = eye(length(tX(1,:))); % Creation of an identity matrix of dimention (M+1)
+    I(1,1) = 0;
+    beta = (tX'*tX + lambda*I)\(tX'*y);
+    
 end
 
