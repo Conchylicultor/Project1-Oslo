433962c1839ddae483b21e9e53a6e9ab06fb94f9
Conchylicultor <etiennefg.pot@gmail.com>
2015-10-18 14:58:26 +0200

    Normalisation

--- main.m
+++ main.m
@@ -5,8 +5,45 @@ close all;
 
 % Loading data
 disp('Project1 - Oslo Team');
+load('Oslo_regression.mat')
 
 % Normalizing the data
+% TODO: Do we have to normalized binary data (0.85, -1.17) ???
+
+meanX = zeros(1, length(X_train(1,:)));
+stdX = zeros(1, length(X_train(1,:)));
+for i = 1:length(X_train(1,:))
+  meanX(i) = mean(X_train(:,i));
+  stdX(i) = std(X_train(:,i));
+  
+  X_train(:,i) = (X_train(:,i)-meanX(i))/stdX(i);
+  % We normalize our testing data with the same value that for our training
+  % (using the same mean and std that for the testing)
+  X_test(:,i) = (X_test(:,i)-meanX(i))/stdX(i);
+end
+
+% TODO: We normalize our training data
+
+%for i = 1:length(X(1,:))
+%  meanX = mean(X(:,i));
+%  X(:,i) = (X(:,i)-meanX)/std(X(:,i));
+%end
+
+% Randomly sorting the data
+
+% for randomly sort the data x times
+%   for divide the data in k part
+%       for dimention of the regretion (first degree, second degree ?)
+%           for cross validation (compute value of lambda)
+% TODO: Estimate complexity
+
+% Divide the data in K-parts
+
+% Compute a lot of RMSE
+
+% Plot the histogram of all RMSE
+
+% Revert the normalization to obtain the correct results
 
 % Ending program
 disp('Thanks for using our script');
